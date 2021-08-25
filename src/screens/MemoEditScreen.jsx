9ea568245fd8c40f228222cc11aaad3a0c4a77f5
Akiyoshi <zundokokiyoshi999@gmail.com>
2021-08-25 20:47:00 +0900

    Update Memodata on MemoEditScreen

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -1,23 +1,59 @@
-import React from 'react'
-import { StyleSheet, TextInput, View } from 'react-native'
+import { shape, string } from 'prop-types'
+import React, { useState } from 'react'
+import { Alert, StyleSheet, TextInput, View } from 'react-native'
+import firebase from 'firebase'
+
 import CircleButton from '../components/CircleButton'
 import KeyboardSafeView from '../components/KeyBoradSafeView'
 
 const MemoEditScreen = (props) => {
-  const { navigation } = props
+  const { navigation, route } = props
+  const { id, bodyText } = route.params
+  const [body, setBody] = useState(bodyText)
+
+  const handlePress = () => {
+    const { currentUser } = firebase.auth()
+    if (currentUser) {
+      const db = firebase.firestore()
+      const ref = db.collection(`users/${currentUser.uid}/memos`).doc(id)
+      ref
+        .set(
+          {
+            bodyText: body,
+            updatedAt: new Date()
+          },
+          { merge: true }
+        )
+        .then(() => {
+          navigation.goBack()
+        })
+        .catch((err) => {
+          Alert.alert(err.code)
+        })
+    }
+  }
+
   return (
     <KeyboardSafeView style={styles.container} behavior="height">
       <View style={styles.inputContainer}>
-        <TextInput value="買い物リスト" multiline style={styles.input} />
+        <TextInput
+          value={body}
+          multiline
+          style={styles.input}
+          onChangeText={(text) => {
+            setBody(text)
+          }}
+        />
       </View>
-      <CircleButton
-        name="check"
-        onPress={() => {
-          navigation.goBack()
-        }}
-      />
+      <CircleButton name="check" onPress={handlePress} />
     </KeyboardSafeView>
   )
+}
+
+MemoEditScreen.propTypes = {
+  route: shape({
+    params: shape({ id: string, bodyText: string })
+  }).isRequired
 }
 
 const styles = StyleSheet.create({
